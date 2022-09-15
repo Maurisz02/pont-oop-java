0722e5f2dedf22eff58a25344ae619c1e9424441
Maurisz <p2002adam@gmail.com>
2022-09-15 09:00:43 +0200

    pont first fe task done

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -1,8 +1,16 @@
 package hu.petrik.pontoop;
 
+import hu.petrik.pontoop.pont.Pont;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Pont p1 = new Pont();
+        Pont p2 = new Pont(0,0);
+        Pont p3 = new Pont(3,4);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
