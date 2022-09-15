a239cc8eb5a0e91280e2dd5044526256dad5c01e
Maurisz <p2002adam@gmail.com>
2022-09-15 09:19:38 +0200

    10 random coordinate

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -8,9 +8,23 @@ public class Main {
 	    Pont p1 = new Pont();
         Pont p2 = new Pont(0,0);
         Pont p3 = new Pont(3,4);
+        Pont p4 = new Pont (100);
 
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);
+
+        Pont[] pontok = new Pont[10]; // 80 byte
+
+        for (int i = 0 ; i< pontok.length; i++){
+            pontok[i] = new Pont (100);
+        }
+        for(int i = 0 ; i< pontok.length; i++){
+            System.out.print(pontok[i]+", ");
+        }
+        for(Pont p: pontok){// foreach in java
+            System.out.println(p);
+        }
     }
 }
